6e3470281fb77af4452def86d6b9eaa11991018d
PieroSotelo <piero.sotelo@vallegrande.edu.pe>
2026-05-20 08:23:14 -0500

    feat(Login):Mejorando la sección del login

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,12 +12,28 @@ import { RouterLink } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
 
   showMenu=false;
+  nombreUsuario: string = '';
+  cargoUsuario: string = '';
+  Date = Date;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.nombreUsuario = localStorage.getItem('userNombre') || 'Usuario';
+    this.cargoUsuario = localStorage.getItem('userCargo') || 'Trabajador';
+  }
 
   toggleMenu(){
     this.showMenu=!this.showMenu;
+  }
+
+  logout() {
+    localStorage.removeItem('userNombre');
+    localStorage.removeItem('userCargo');
+    this.router.navigate(['/login']);
   }
 
 }
--- src/app/layout/topbar/topbar.component.ts
+++ src/app/layout/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector:'app-topbar',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl:'./topbar.component.html',
   styleUrls:['./topbar.component.css']
 })
-export class TopbarComponent{}
+export class TopbarComponent implements OnInit {
+  nombreUsuario: string = '';
+
+  ngOnInit() {
+    this.nombreUsuario = localStorage.getItem('userNombre') || 'Usuario';
+  }
+}
